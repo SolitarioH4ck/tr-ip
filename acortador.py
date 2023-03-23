e9b62579a9ddaf9bcc257b6700687035a48ab253
SolitarioH4ck <128333029+SolitarioH4ck@users.noreply.github.com>
2023-03-22 18:43:29 -0600

    Create acortador.py

---
+++ acortador.py
@@ -0,0 +1,16 @@
+#Modulos
+import pyshorteners,time
+from os import system
+#by: SolitarioH4ck23
+
+#Colores
+red = '\033[1;31m'
+white = '\033[1;39m'
+
+system("clear")
+print('')
+url = input(f"{red}Ingresa la url >> {white}")
+shrt = pyshorteners.Shortener()                       nurl = shrt.tinyurl.short(url)
+print('')
+time.sleep(1.2)
+print(f"{red}URL Acortada:{white}",nurl)
