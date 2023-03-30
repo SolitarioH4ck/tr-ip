8f3467bd8787af448a535865adda1c268e00b06d
SolitarioH4ck <128333029+SolitarioH4ck@users.noreply.github.com>
2023-03-29 22:54:50 -0600

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,23 @@
+import requests, time
+#code by: SolitarioH4ck23
+
+#Colores..
+red = '\033[1;31m'
+white = '\033[1;39m'
+azul = '\033[1;34m'
+
+numero = input(f"{white}[{red}-{white}]{white}Numero de la victima: {red}")
+print(f'{white}Escribe el Mensaje')
+
+sms =input(f">> {red}")
+
+resp = requests.post('https://textbelt.com/text', {
+   'phone': f'{numero}',
+    'message': f'{sms}',
+    'key': 'textbelt',
+      })
+print(f'{white}Enviado SMS...')
+time.sleep(1.5)
+print('')
+input(f'{white}[{red}-{white}] SMS Enviado Presiona enter...')
+system("bash tr.sh")
